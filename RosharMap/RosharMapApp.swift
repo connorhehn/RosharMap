8b3f1537ff9a023ac9dedf054408ad1541e7f3a9
ConnorHehn <connor.hehn@gmail.com>
2025-04-14 20:54:16 -0400

    Initial Commit

---
+++ RosharMap/RosharMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RosharMapApp.swift
+//  RosharMap
+//
+//  Created by Connor Hehn on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct RosharMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
